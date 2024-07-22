fed61e6a9aef1174b63945c883a5602beb20b74e
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2024-07-22 15:23:14 +0300

    Respond to PR feedback.
    Signed-off-by: Nashwan Azhari <nazhari@cloudbasesolutions.com>

--- k8s_test_harness/util/platform_util.py
+++ k8s_test_harness/util/platform_util.py
@@ -12,22 +12,12 @@ import platform
 ROCKCRAFT_PLATFORM_AMD64 = "amd64"
 ROCKCRAFT_PLATFORM_I386 = "i386"
 ROCKCRAFT_PLATFORM_ARM64 = "arm64"
-ROCKCRAFT_PLATFORM_ARMHF = "armhf"
-ROCKCRAFT_PLATFORM_S390X = "s390x"
-ROCKCRAFT_PLATFORM_PPC64EL = "ppc64el"
-ROCKCRAFT_PLATFORM_RISCV64 = "riscv64"
-
-# NOTE(aznashwan): these constants are automatically derived at runtime with
-# no static definition for them, so we must maintain the definitions ourselves:
-PYTHON_MACHINE_X86 = "x86"
-PYTHON_MACHINE_X86_64 = "x86_64"
-PYTHON_MACHINE_ARM64 = "arm64"
 
 
 _PYTHON_MACHINE_TO_ROCKCRAFT_PLATFORM_ARCHITECTURE_MAP = {
-    PYTHON_MACHINE_X86: ROCKCRAFT_PLATFORM_I386,
-    PYTHON_MACHINE_X86_64: ROCKCRAFT_PLATFORM_AMD64,
-    PYTHON_MACHINE_ARM64: ROCKCRAFT_PLATFORM_ARM64
+    "x86": ROCKCRAFT_PLATFORM_I386,
+    "x86_64": ROCKCRAFT_PLATFORM_AMD64,
+    "arm64": ROCKCRAFT_PLATFORM_ARM64
 }
 
 
@@ -44,8 +34,7 @@ def get_current_rockcraft_platform_architecture() -> str:
     machine = platform.machine()
     if not machine:
         raise OSError(
-            "Failed to get current platform through `platform.machine()` "
-            f"which returned: {machine}")
+            "Failed to get current platform through `platform.machine()`.")
 
     if machine not in _PYTHON_MACHINE_TO_ROCKCRAFT_PLATFORM_ARCHITECTURE_MAP:
         raise ValueError(
